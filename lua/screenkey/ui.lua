cc830750de70328c2bd3d845e349e7cc456b8892
NStefan002 <n.stefan002@gmail.com>
2025-08-13 00:45:03 +0200

    feat(ui): set `winfixbuf` for `screenkey` window

--- lua/screenkey/ui.lua
+++ lua/screenkey/ui.lua
@@ -52,6 +52,7 @@ function M:open_win()
     self.active = true
     utils.clear_buf_lines(vim.g.screenkey_bufnr, 0, config.options.win_opts.height)
     api.nvim_set_option_value("filetype", "screenkey", { buf = vim.g.screenkey_bufnr })
+    api.nvim_set_option_value("winfixbuf", true, { win = vim.g.screenkey_winnr })
     self.set_highlights()
     api.nvim_win_set_hl_ns(vim.g.screenkey_winnr, vim.g.screenkey_ns_id)
 
