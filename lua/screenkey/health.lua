14163df0c8ec7d0f1a55940e5861b7edd3629adc
NStefan002 <n.stefan002@gmail.com>
2026-03-31 01:24:42 +0200

    feat(health): check for Neovim version

--- lua/screenkey/health.lua
+++ lua/screenkey/health.lua
@@ -3,6 +3,12 @@ local M = {}
 function M.check()
     local config = require("screenkey.config")
     vim.health.start("screenkey.nvim")
+    local has_nvim_version = vim.fn.has("nvim-0.11.0") == 1
+    if has_nvim_version then
+        vim.health.ok("Neovim version >= 0.11.0")
+    else
+        vim.health.error("Neovim version < 0.11.0")
+    end
     local ok, err = config.validate_config(config.options)
     if ok then
         vim.health.ok("Setup is correct")
